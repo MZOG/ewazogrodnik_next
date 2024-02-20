b81a03d4436cca85d6bbae3c18bad0d8f71f0e19
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2024-02-20 19:12:12 +0100

    homepage

--- src/components/latest-works.js
+++ src/components/latest-works.js
@@ -24,21 +24,21 @@ export default async function LatestWorks() {
   const { data: { allWorks } } = await performRequest({ query: WORKS_QUERY });
 
   return (
-    <Container type="section" customClass="mt-14">
-    <h2 className={`${serif_display.className} text-3xl`}>Latest works</h2>
+    <Container type="section">
+    <h2 className={`${serif_display.className} text-3xl text-center`}>Latest works</h2>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-14">
+    <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-5 mt-14">
       {allWorks.map((work, index) => {
         const { slug, title, mainImage: { responsiveImage } } = work
         return (
-          <Link href={`/work/${slug}`} key={index}>
+          <Link href={`/work/${slug}`} key={index} className="mb-5 block">
             <Image 
               alt={title} 
               sizes={responsiveImage.sizes} 
               src={responsiveImage.src} 
               width={responsiveImage.width} 
               height={responsiveImage.height}
-              className="rounded-xl"
+              className="rounded-xl h-auto max-w-full"
             />
           </Link>
         )
