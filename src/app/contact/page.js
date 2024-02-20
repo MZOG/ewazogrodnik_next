c3594679c026d48ef9411b4b81a6788b63f73a28
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2024-02-20 22:41:20 +0100

    contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,36 @@
+import Container from "@/components/common/container";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { serif_display } from "@/lib/fonts";
+
 export default async function Contact() {
   return (
-    <p>contact page</p>
+    <Container type="section" maxWidth="max-w-2xl" customClass="bg-rose-200/40 rounded-xl py-10">
+
+      <h1 className={`${serif_display.className} text-3xl text-center mb-10`}>Contact me</h1>
+
+      <div className="flex flex-col items-center gap-7 justify-center">
+        <div className="grid w-full max-w-xs items-center gap-1.5">
+          <Label htmlFor="name">Your name</Label>
+          <Input type="text" id="name" placeholder="Full name" className="border-rose-200 placeholder:text-primary/80" />
+        </div>
+
+        <div className="grid w-full max-w-xs items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Email" className="border-rose-200 placeholder:text-primary/80" />
+        </div>
+
+        <div className="grid w-full max-w-xs items-center gap-1.5">
+          <Label htmlFor="message">Message</Label>
+          <Textarea id="message" className="border-rose-200 placeholder:text-primary/80 resize-none" />
+        </div>
+
+        <Button variant="rose">
+          Send message
+        </Button>
+      </div>
+    </Container>
   )
 }
